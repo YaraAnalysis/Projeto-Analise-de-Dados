802f1649518797cf42a11946d5bf9eefebd0587c
YaraAnalysis <114318856+YaraAnalysis@users.noreply.github.com>
2023-02-04 21:17:04 -0300

    Import Databases

--- Análise-de-Dados.py
+++ Análise-de-Dados.py
@@ -1,1 +1,6 @@
 import pandas as pd
+#importando os arquivos
+vendas_df = pd.read_csv(r'Contoso - Vendas - 2017.csv', sep=';')
+produtos_df = pd.read_csv(r'Contoso - Cadastro Produtos.csv', sep=';')
+lojas_df = pd.read_csv(r'Contoso - Lojas.csv', sep=';')
+clientes_df = pd.read_csv(r'Contoso - Clientes.csv', sep=';')
