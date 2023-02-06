ca6c29e42ef9f64e32f6af56659dae8cfda09e1f
YaraAnalysis <114318856+YaraAnalysis@users.noreply.github.com>
2023-02-06 19:59:41 -0300

    Correção do output do cliente
    Melhoria no output do cliente que mais comprou. Incluí o nome e sobrenome.

--- Análise-de-Dados.py
+++ Análise-de-Dados.py
@@ -7,7 +7,7 @@ lojas_df = pd.read_csv(r'Contoso - Lojas.csv', sep=';')
 clientes_df = pd.read_csv(r'Contoso - Clientes.csv', sep=';')
 
 #limpando apenas as colunas que queremos
-clientes_df = clientes_df[['ID Cliente', 'E-mail']]
+clientes_df = clientes_df[['ID Cliente', 'Primeiro Nome', 'Sobrenome', 'E-mail']]
 produtos_df = produtos_df[['ID Produto', 'Nome do Produto']]
 lojas_df = lojas_df[['ID Loja', 'Nome da Loja']]
 
@@ -18,8 +18,8 @@ vendas_df = vendas_df.merge(clientes_df, on='ID Cliente').rename(columns={'E-mai
 #print(vendas_df)
 
 #Qual cliente que comprou mais vezes?
-frequencia_clientes = vendas_df['E-mail do Cliente'].value_counts()
+frequencia_clientes = vendas_df[['Primeiro Nome', 'Sobrenome', 'E-mail do Cliente']].value_counts()
 #print(frequencia_clientes)
 #frequencia_clientes[:5].plot(figsize=(15, 5), yticks=range(1, 100, 5))
 print('\nO cliente que comprou mais vezes foi:')
-print(frequencia_clientes[:2])
+print(frequencia_clientes[:1])
